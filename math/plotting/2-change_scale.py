cf0dff98a40700898a3b4a28980221a516d74807
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-11 23:28:46 +0200

    Exponential Decay of C-14

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 28651, 5730)
+r = np.log(0.5)
+t = 5730
+y = np.exp((r / t) * x)
+
+# your code here
+
+plt.xlabel("Time (years)") # This line shows time in yeaars on x-axis
+plt.ylabel("Fraction Remaining") # This line shows fraction remaining on y-axis
+plt.title("Exponential Decay of C-14") # The title
+plt.plot(x,y) # Plots x and y
+plt.show()
