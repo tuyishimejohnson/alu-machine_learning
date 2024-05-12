86fde09cef4623aba8df3d6262e881ad11e8528d
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-12 10:46:11 +0200

    All in One

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
-plt.ylabel('Quantity of Fruit')
+plt.ylabel("Quantity of Fruits")
 plt.yticks(np.arange(0, 81, 10))
-plt.title('Number of Fruit per Person')
+plt.title("Number of Fruit per Person")
+
+people = ['Farrah', 'Fred', 'Felicia']
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+fruit_names = ['apples', 'bananas', 'oranges', 'peaches']()
+
+
+plt.bar(people, fruit[0], color=colors[0], width=0.5, label=fruit_names[0])
+plt.bar(people, fruit[0])
+plt.bar(people, fruit[1], bottom=fruit[0], color=colors[1], width=0.5, label=fruit_names[1])
+plt.bar(people, fruit[2], bottom=np.sum(fruit[:2], axis=0), color=colors[2], width=0.5, label=fruit_names[2])
+plt.bar(people, fruit[3], bottom=np.sum(fruit[:3], axis=0), color=colors[3], width=0.5, label=fruit_names[3])
+
+
+plt.legend()
+plt.show()
