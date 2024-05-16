2f8b649ebfe6a4d9039671e8307d39cbff391975
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-16 23:49:23 +0200

    Linear Algebra project

---
+++ math/linear_algebra/0-slice_me_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 =  arr.slice(0,2)
+arr2 =  arr.slice(4)
+arr3 =  arr.slice(1, 6)
+
+
+print(f"The first two numbers of the array are: {arr1}")
+print(f"The last five numbers of the array are: {arr2}")
+print("The 2nd through 6th numbers of the array are: {arr3}")
