b4744705b656078b42cdf8c3ad5f2085d901d71f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-17 06:56:59 +0200

    Solved the issue of the second array

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,11 @@
+
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 =  slice(0, 2)
-arr2 =  slice(4)
+arr2 =  slice(4,None)
 arr3 =  slice(1, 6)
 print("The first two numbers of the array are: {}".format(arr[arr1]))
 print("The last five numbers of the array are: {}".format(arr[arr2]))
 print("The 2nd through 6th numbers of the array are: {}".format(arr[arr3]))
+
+# This is it
