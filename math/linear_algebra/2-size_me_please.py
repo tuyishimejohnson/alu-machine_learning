0f1e6d1d62f1a294910153220934e88262bd0705
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 20:28:38 +0200

    Added the condition for one dimensional array

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,16 +1,23 @@
+
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
     new_array = []
     rows = len(matrix)
+    print(rows)
     columns = len(matrix[0])
     
+
     if columns <= 2 and rows <= 2:
         new_array = [rows, columns]
     else:
         for m in matrix:
-            for i in m:
-                new_array = [rows, columns, len(i)]        
+            if rows == 1:
+                new_array = [rows, len(m)]
+                break
+            else:
+                for i in m:
+                    new_array = [rows, columns, len(i)]        
     return new_array
 result = matrix_shape([[1, 2], [3, 4]])
 print(result)
