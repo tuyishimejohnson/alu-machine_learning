72620346f1f99b55bffae6d93bb5466e59365211
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 20:12:57 +0200

    A function that calculates the shape of the matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,24 +1,16 @@
-
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
     new_array = []
     rows = len(matrix)
     columns = len(matrix[0])
-    print(columns)
-    print(rows)
-
+    
     if columns <= 2 and rows <= 2:
         new_array = [rows, columns]
     else:
         for m in matrix:
             for i in m:
-                new_array = [rows, columns, len(i)]
-            
-            
+                new_array = [rows, columns, len(i)]        
     return new_array
-
 result = matrix_shape([[1, 2], [3, 4]])
-
-
 print(result)
