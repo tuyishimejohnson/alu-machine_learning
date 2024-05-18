92947776a10ed7cd2d5762c2a2a3db3a55fd0dc3
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 13:48:38 -0700

    Transpose of a matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-def matrix_transpose(matrix):
 
+def matrix_transpose(matrix):
+    """ function that returns the transpose of 2d matrix
     """
-    a function that returns the transpose of a 2D matrix.
-    """
-   transposed = [[matrix[row][col] for row in range(len(matrix))]
+    transposed = [[matrix[row][col] for row in range(len(matrix))]
                   for col in range(len(matrix[0]))]
-	      return transposed
+    return transposed
 
 
 if __name__ == "__main__":
-	matrix_transpose = __import__('3-flip_me_over').matrix_transpose
+    matrix_transpose = __import__('3-flip_me_over').matrix_transpose
