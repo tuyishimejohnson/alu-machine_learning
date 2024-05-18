0973f40d65c4d2175b1386f5430ed634f0032309
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 15:23:47 -0700

    Returns a matrix from the concatenated arrays

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -11,14 +11,13 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1[0]) != len(mat2[0]):
             return None
         return mat1 + mat2
-    elif axis == 1:       
+    elif axis == 1:
         if len(mat1) != len(mat2):
             return None
         return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
         return None
 
-    
+
 if __name__ == "__main__":
     cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
-
