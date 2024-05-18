4e25b6f2b3ca03d57f2a1079aabfeabc122a4ca8
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 17:45:48 +0200

    A function that calculates the shape of the matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,25 +1,10 @@
-
 #!/usr/bin/env python3
 import numpy as np
-mat1 = [[1, 2], [3, 4]]
-
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-m = np.array(mat1)
-n=np.array(mat2)
-print(m.shape)
-print(n.shape)
-
 
 def matrix_shape(matrix):
-    m = np.array(matrix)
+    m=np.array(matrix)
     return m.shape
 
-
-
-
-"""mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))"""
+result = matrix_shape( [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+print(list(result))
