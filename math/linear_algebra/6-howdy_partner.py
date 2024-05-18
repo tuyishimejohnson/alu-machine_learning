14fc8a678a1f6c51f83b506b059f4c4439b95039
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 14:56:14 -0700

    Concatenating two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def cat_arrays(arr1, arr2):
+    """A function that concatenates two arrays and return a new list"""
+
+    return arr1 + arr2
+
+
+if __name__ == "__main__":
+    cat_arrays = __import__('6-howdy_partner').cat_arrays
