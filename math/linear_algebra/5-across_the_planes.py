20c90dabd2318e692faf38249718be332725d39e
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 14:39:28 -0700

    Add two arrays and return a matrix

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,17 +6,14 @@ def add_matrices2D(mat1, mat2):
         return None
     for row1, row2 in zip(mat1, mat2):
         if len(row1) != len(row2):
-            return None
-
-    
+            return None    
     result = []
     for row1, row2 in zip(mat1, mat2):
         new_row = [
             elem1 + elem2 
             for elem1, elem2 in zip(row1, row2)
         ]
-        result.append(new_row)
-    
+        result.append(new_row)    
     return result
 
 
