31bed12b8bf03e13be68ccdf1d9fccab6b0860bc
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 16:45:48 -0700

    Function to calculate the shape of a numpy arrr

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -9,7 +9,3 @@ mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
 print(np_shape(mat1))
 print(np_shape(mat2))
 print(np_shape(mat3))
-
-
-if __name__ == "__main__":
-    np_shape = __import__('10-ill_use_my_scale').np_shape
