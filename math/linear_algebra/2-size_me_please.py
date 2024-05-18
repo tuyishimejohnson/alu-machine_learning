7f2c4cd12377d44120250f525eb023aa989526d1
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 20:47:31 +0200

    Function to print the shape of the matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,7 +4,7 @@ def matrix_shape(matrix):
     while isinstance(matrix, list):
         new_array.append(len(matrix))
         matrix = matrix[0]
-    print("OK")
+    return "OK"
 
 result = matrix_shape([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
         [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
