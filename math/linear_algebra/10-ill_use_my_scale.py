6a08e9cec6a90b5297ba0382c7a6c28dfb301614
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 16:54:44 -0700

    Function to calculate the shape of a numpy arrr

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""A function that returns the shape of numpy arrays"""
+
 
 def np_shape(matrix):
+    """
+    matrix.shape: which is the shape of the matrix
+    """
     return matrix.shape
