a0c2320989cf6d10971eb72fb8992b24fa305e51
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-19 00:58:32 +0200

    Making changes

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,10 @@
+
 #!/usr/bin/env python3
+
+"""
+A function that returns the shape of an array.
+
+"""
 def matrix_shape(matrix):
     new_array = []
     while isinstance(matrix, list):
@@ -10,6 +16,9 @@ def matrix_shape(matrix):
         
     return new_array
     
+
+if __name__ == "__main__":
+    matrix_shape_function = __import__('2-size_me_please').matrix_shape
 
 result = matrix_shape([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
         [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
