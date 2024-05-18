40333ed0a1cd1b46c15902596f80444c07276582
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-18 14:05:46 -0700

    Sum of two arrays

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-""" function that returns the surm of two arrays in matrix
-    """
+!/usr/bin/env python3
 
+"""A function that returns the sum of two arrays in matrix"""
 def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
     else:
-        for (a, b) in zip(arr1, arr2):
-            return [a + b]
+        return [a + b for a, b in zip(arr1, arr2)]
 
 
 
