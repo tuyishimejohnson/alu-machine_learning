38f96f536266f3b6541788419f96d93b6a0a7e50
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-19 08:21:42 -0700

    Transpose matrix using numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+"""a function that transposes a matrix  """
+
+
+def np_transpose(matrix):
+    """
+    np: shorthand of the module numpy
+    matrix: argument that has matrices
+    """
+    return np.transpose(matrix)
