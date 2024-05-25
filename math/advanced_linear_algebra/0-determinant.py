057c233e06f7c10be35ba25b3a2a48dd406d12eb
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-25 01:01:54 -0700

    Calculating determinant of a matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -26,25 +26,3 @@ def determinant(matrix):
     return det
 
 if __name__ == '__main__':
-    mat0 = [[]]
-    mat1 = [[5]]
-    mat2 = [[1, 2], [3, 4]]
-    mat3 = [[1, 1], [1, 1]]
-    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-    mat5 = []
-    mat6 = [[1, 2, 3], [4, 5, 6]]
-
-    print(determinant(mat0))  # Output: 1
-    print(determinant(mat1))  # Output: 5
-    print(determinant(mat2))  # Output: -2
-    print(determinant(mat3))  # Output: 0
-    print(determinant(mat4))  # Output: 192
-    try:
-        determinant(mat5)
-    except Exception as e:
-        print(e)  # Output: matrix must be a square matrix
-    try:
-        determinant(mat6)
-    except Exception as e:
-        print(e)  # Output: matrix must be a square matrix
-
