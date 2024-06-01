681bf5e866db8e1f1f1b6f75e1ec5fe016a873f8
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-01 08:09:53 -0700

    Integral of polynomial

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
-"""A function that calculates integral of polynomial"""
-
 
 def poly_integral(poly, C=0):
     """
     c: integration constant
     if poly or c are not valid: return value is None
     """
-    if not isinstance(poly, list) or not all(isinstance(coef, (int, float)) for coef in poly):
+    if type(poly) is not list or len(poly) < 1:
         return None
-    if not isinstance(C, int):
+    if type(C) is not int and type(C) is not float:
         return None
-    integral = [coef / (i + 1) for i, coef in enumerate(poly)]
-    integral.insert(0, C)  # Add the integration constant
-
+    for coefficient in poly:
+        if type(coefficient) is not int and type(coefficient) is not float:
+            return None
+    if type(C) is float and C.is_integer():
+        C = int(C)
+    integral = [C]
+    for power, coefficient in enumerate(poly):
+        if (coefficient % (power + 1)) is 0:
+            new_coefficient = coefficient // (power + 1)
+        else:
+            new_coefficient = coefficient / (power + 1)
+        integral.append(new_coefficient)
+    while integral[-1] is 0 and len(integral) > 1:
+        integral = integral[:-1]
     return integral
