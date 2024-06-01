811c152b9cdb3d7a9d6171a2529712a51878fcd4
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-01 07:17:05 -0700

    Derivative of polynomial

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 """A function that calculates a derivative of polynomial"""
 
 
@@ -12,21 +11,20 @@ def poly_derivative(poly):
     check2: checking if coef is a number
     next step: calculating the derivative
     """
-    
-    try:
-        if type(poly) is not list:
-            return None
-        for c in poly:
-            c + 0
-    except TypeError:
+
+    if type(poly) is not list:
         return None
+    
+    for coef in poly:
+        try:
+            coef + 0
+        except TypeError:
+            return None
     if len(poly) == 1:
         return [0]
+    derivative = [i * poly[i] for i in range(1, len(poly))]
 
-    der = [i * poly[i] for i in range(1, len(poly))]
-
-    
-    if all(c == 0 for c in der):
+    if all(coef == 0 for coef in derivative):
         return [0]
 
-    return der
+    return derivative
