8516e526a99c4464477967feddaa5c76d1d7f2ba
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-01 06:10:51 -0700

    Summation of i squared

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-
 """A function that calculates the summation of i squared. """
 
 def summation_i_squared(n):
     """
     n: stopping condition
     return: integer value of the sum
-    if n is not a number: None    
+    if n is not a number: None
+    try: checking whether n is an integer 
     """
+    try:
+        integer = int(n)
+        if integer <= 0:
+            return None
+    except(ValueError):
 
-    if type(n) != int and n <= 0:
         return None
-    
     return (n*(n + 1)*((2*n) + 1))//6
