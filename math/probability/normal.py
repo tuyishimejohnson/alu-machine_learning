08396579c9b8c269b103989337e1b9d98e158c71
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-09 11:04:06 +0200

    Calculates the normal distribution.

---
+++ math/probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""
+A class that represents a normal distribution.
+"""
+
+
+class Normal:
+
+    """ 
+    Calculate the normal distribution
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum((x - self.mean) ** 2 for x in data) / len(data)) ** 0.5
