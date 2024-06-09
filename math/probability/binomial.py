9913f81b96ef829983b5abe2cb3e2bd24ebf3ee9
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-09 11:48:57 +0200

    Binomial distribution.

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -4,23 +4,17 @@
 """ 
 A class that represents binomial distribution.
 """
-class Binomial:
-    def __init__(self, data=None, n=1, p=0.5):
-        """ 
-        data is a list of the data to be used to estimate the distribution
-        n is the number of Bernoulli trials
-        p is the probability of a “success”
-        Sets the instance attributes n and p
-        """
-        if data is None:
-            if not isinstance(n, int) or n <= 0:
-                raise ValueError("n must be a positive value")
-            if not isinstance(p, (float, int)) or not (0 < p < 1):
-                raise ValueError("p must be greater than 0 and less than 1")
 
-            self.n = n
-            self.p = float(p)
-        else:
+
+class Binomial:
+    """ 
+    data is a list of the data to be used to estimate the distribution
+    n is the number of Bernoulli trials
+    p is the probability of a “success”
+    """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        if data is not None:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
@@ -28,12 +22,62 @@ class Binomial:
 
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
-            
-            p_initial = 1 - (variance / mean)
-            n_initial = round(mean / p_initial)
-            
-            self.n = n_initial
-            self.p = mean / self.n
 
-    def __str__(self):
-        return f'n: {self.n} p: {self.p:.3f}'
+            p = 1 - (variance / mean)
+            n = round(mean / p)
+            p = mean / n
+
+            self.n = n
+            self.p = float(p)
+        else:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+
+            self.n = int(n)
+            self.p = float(p)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes
+
+        k = number of “successes”
+        If k is not an integer, convert it to an integer
+        If k is out of range, return 0
+        Returns the PMF value for k
+
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        factorial_k = 1
+        for i in range(1, k + 1):
+            factorial_k *= i
+        factorial_n = 1
+        for i in range(1, self.n + 1):
+            factorial_n *= i
+        factorial_n_k = 1
+        for i in range(1, self.n - k + 1):
+            factorial_n_k *= i
+        coefficient = factorial_n / (factorial_k * factorial_n_k)
+        return coefficient * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of successes
+
+        Calculates the value of the CDF for a given number of “successes”
+        k = number of “successes”
+        If k is not an integer, convert it to an integer
+        If k is out of range, return 0
+        Returns the CDF value for k
+
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
