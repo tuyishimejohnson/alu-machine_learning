f8f4f222cbf6d79d68b2b41aa96470420a7d0969
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-15 22:11:15 +0200

    A class that represents multivariate normal distribution.

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""
+A class that represents a Multivariate Normal distribution
+"""
+
+
+class MultiNormal:
+
+    """
+    n: the number of data points
+    d: the number of dimensions in each data point
+    If data is not a 2D numpy.ndarray: TypeError> data must be a 2D numpy.ndarray
+    If n < 2, ValueError: data must contain multiple data points
+    """
+    def __init__(self, data):
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+
+        d, n = data.shape
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
+
+        deviation = data - self.mean
+        self.cov = deviation @ deviation.T / (n - 1)
