aeec02e08b577523a7e0f1681945e1d7e0dbfaf8
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-15 20:04:59 +0200

    Chore: Created a directory for covariance matrix.

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def mean_cov(X):
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+
+    n = X.shape[0]
+    mean = np.mean(X, axis=0).reshape(1, -1)
+
+    X = X - mean
+    cov = np.dot(X.T, X) / (n - 1)
+
+    return mean, cov
