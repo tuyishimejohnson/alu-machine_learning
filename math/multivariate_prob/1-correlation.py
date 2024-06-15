2e0ee9944e3674c8f8bd4b6b4857ce03b1ce04a9
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-15 21:30:49 +0200

    Correlation matrix

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,26 @@
+#/usr/bin/env python3
+
+"""
+A function that calculates the correlation matrix.
+"""
+import numpy as np
+
+
+def correlation(C):
+    """
+    d: number of dimensions
+    c: contains covariance matrix
+
+    returns:
+    TypeError: C must be a numpy.ndarray
+    ValueError: C must be a 2D square matrix
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    std_dev = np.sqrt(np.diag(C))
+    correlation_matrix = C / np.outer(std_dev, std_dev)
+
+    return correlation_matrix
