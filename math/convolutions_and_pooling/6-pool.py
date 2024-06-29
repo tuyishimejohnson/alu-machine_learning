97776e710432de3d12d675b3c3ac4361a5a39ef7
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-29 18:08:44 +0200

    Pooling

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -2,28 +2,29 @@
 
 import numpy as np
 
+
 def pool(images, kernel_shape, stride, mode='max'):
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
-    
+
     output_h = (h - kh) // sh + 1
     output_w = (w - kw) // sw + 1
-    
+
     pooled = np.zeros((m, output_h, output_w, c))
-    
+
     for i in range(output_h):
         for j in range(output_w):
-            x_start = i * sh
-            x_end = x_start + kh
-            y_start = j * sw
-            y_end = y_start + kw
-            
+            x_st = i * sh
+            x_end = x_st + kh
+            y_st = j * sw
+            y_end = y_st + kw
+
             if mode == 'max':
-                pooled[:, i, j, :] = np.max(images[:, x_start:x_end, y_start:y_end, :], 
+                pooled[:, i, j, :] = np.max(images[:, x_st:x_end, y_st:y_end, :],
                                             axis=(1, 2))
             elif mode == 'avg':
-                pooled[:, i, j, :] = np.mean(images[:, x_start:x_end, y_start:y_end, :], 
+                pooled[:, i, j, :] = np.mean(images[:, x_st:x_end, y_st:y_end, :],
                                              axis=(1, 2))
-                
+        
     return pooled
