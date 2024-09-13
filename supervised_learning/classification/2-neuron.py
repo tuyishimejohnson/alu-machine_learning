9aac25f1ffc65add11dbdc2e7d6b50f23b8b11e0
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-13 13:53:06 +0200

    chore: forward propagation

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+""" A class Neuron that defines a single neuron
+     for binary classification """
+
+import numpy as np
+
+
+class Neuron:
+    """
+        Initializes a constructor with nx variable
+        to make private instance attributes
+        Parameters:
+        nx: integer
+        W: weight of neuron
+        A: activated output
+        b: bias of neuron
+        Raises TypeError
+        Raises ValueError
+        """
+    def __init__(self, nx):
+        self.nx = nx
+
+        if not isinstance(self.nx, int):
+            raise TypeError("nx must be an integer")
+        if self.nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ private getter for weight  """
+        return self.__W
+
+    @property
+    def b(self):
+        """ private getter for bias """
+        return self.__b
+
+    @property
+    def A(self):
+        """ private getter for activation output """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ A function to calculate the forward propagation """
+        self.X = np.ndarray(self.nx, m=None)
+
+    def set_A(self):
+        return 1/(1 + np.exp(-self.nx))
+
+    
