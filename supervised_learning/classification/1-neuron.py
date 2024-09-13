49357a069cb7244f627f3cc94a9341886ab582b4
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-13 13:18:54 +0200

    chore: add private attributes

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" A class Neuron that defines a single neuron
+     for binary classification """
+
+import numpy as np
+
+
+class Neuron:
+    """
+        Initializes a constructor with nx variable
+        to make private instance attributes
+        Parameters:
+        nx: integer
+        W: weight of neuron
+        A: activated output
+        b: bias of neuron
+        """
+    def __init__(self, nx):
+        self.nx = nx
+
+        if not isinstance(self.nx, int):
+            raise TypeError("nx must be an integer")
+        if self.nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
