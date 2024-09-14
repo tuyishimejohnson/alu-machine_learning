a23bc09d641a12f68e3bae217ca99142b3035b0a
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-14 18:41:25 +0200

    chore:a method to calculate the gradient descent

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+""" a class for Neural Network
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """ Instantiation function
+
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # getter functions
+    @property
+    def W1(self):
+        """ weights for hidden layer"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """bias one """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """ activated value one """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """ weights for hidden layer two """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """ bias two """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ activated value two """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network.
+        """
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression.
+        """
+        m = Y.shape[1]
+        cost = -np.mean(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        a method that evaluates the prediction of a neural network.
+        """
+        A1, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neural network.
+
+        parameters:
+        X (numpy.ndarray): input data with shape (nx, m).
+        Y (numpy.ndarray): correct labels for the input data with shape (1, m).
+        A1 (numpy.ndarray): output of the hidden layer.
+        A2 (numpy.ndarray): predicted output.
+        alpha (float): the learning rate.
+        """
+        m = X.shape[1]
+
+        # gradients
+        dZ2 = A2 - Y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        # weights and biases
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
