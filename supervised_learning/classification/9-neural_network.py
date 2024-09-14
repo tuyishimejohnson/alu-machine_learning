5063397d7b40206de56b2039e1c7b52f99201638
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-14 15:40:24 +0200

    chore: neural network class with private methods

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+""" a class for Neural Network
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """ Instantiation function
+
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
+
+    @property
+    def W1(self):
+        """ weight of hidden layer """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """ bias one """
+        return self.__b1
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def A1(self):
+        """ activated value one """
+        return self.__A1
+
+    @property
+    def b2(self):
+        """ bias two """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ activated value 2 """
+        return self.__A2
