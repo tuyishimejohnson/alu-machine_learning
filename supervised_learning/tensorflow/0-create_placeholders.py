1cd2401e76b659bdfed0f67341f4746ba350584e
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-20 11:19:08 +0200

    chore: a function to return two placeholders for a neural network

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -8,6 +8,9 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
+    """ A function that is used to calculate the
+    placeholder of two neural networks
+"""
     x = tf.placeholder(tf.float32, shape=[None, nx], name="x")
     y = tf.placeholder(tf.float32, shape=[None, classes], name="y")
 
