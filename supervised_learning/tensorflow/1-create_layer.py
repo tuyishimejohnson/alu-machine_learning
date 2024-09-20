f1c3a78a5994a0d30ddcb58161434b269149c681
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-20 11:36:57 +0200

    chore: a function to return tensor output of the layer in a neural network

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+""" A function that is used to the tensor output of the layer
+"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    args:
+    prev: output of the previous layer
+    n: number of of nodes in the layer to create
+    activation: activation function for a layer to use
+    
+    returns:
+    tensor output
+    """
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    w = tf.Variable(initializer([prev.get_shape().as_list()[1], n]), name='w')
+    b = tf.Variable(tf.zeros([n]), name='b')
+    layer = tf.add(tf.matmul(prev, w), b)
+    if activation is not None:
+        layer = activation(layer)
+    return layer
