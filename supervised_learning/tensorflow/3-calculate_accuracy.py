dfbe44371de1eb445c0ea07e45419ffa4cbc5a92
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-21 10:06:33 +0200

    chore: calculate the accuracy of a prediction

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+""" A function that calculates the accuracy of a prediction
+"""
+
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    args:
+    y: the placeholder for labels of input data
+    y_pred: tensor containing the network’s predictions
+
+    returns:
+    accuracy = correct_predictions / all_predictions
+    """
+    correct_predictions = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    return accuracy
