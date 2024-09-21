cbe6c91f463c08c64341e8ae59c28165f553a923
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-21 10:11:53 +0200

    chore:calculate the loss of a prediction

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" A function that calculates the softmax cross-entropy 
+loss of a prediction
+"""
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    args:
+    y: the placeholder for labels of input data
+    y_pred: tensor containing the network’s predictions
+
+    returns:
+    a tensor containing the loss of the prediction
+    """
+    return tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_pred)
+        )
