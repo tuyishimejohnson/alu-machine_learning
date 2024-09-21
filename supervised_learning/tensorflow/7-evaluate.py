d29e017028d3c0f0ce4c1d0cd2ea8511b3f9e572
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-21 10:45:14 +0200

    evaluate the output of a neural network

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+""" A function that evaluates the output of a neural network
+"""
+import tensorflow as tf
+import numpy as np
+
+
+def create_train_op(loss, alpha):
+    """
+    args:
+    X:numpy.ndarray containing the input data to evaluate
+    Y:numpy.ndarray containing the one-hot labels for X
+    save_path:the location to load the model from
+
+    returns:
+     the network’s prediction, accuracy, and loss, respectively
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
+        ls = sess.run(loss, feed_dict={x: X, y: Y})
+    return pred, acc, ls
