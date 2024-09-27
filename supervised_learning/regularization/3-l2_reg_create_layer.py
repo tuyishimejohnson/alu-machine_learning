f8b66a57125f0ceaf26e87ab4208b4da1ff3a3d2
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-27 11:46:39 +0200

    Create a Layer with L2 Regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -21,7 +21,9 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     layer = tf.layers.Dense(
         units=n,
         activation=activation,
-        kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"),
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
+            mode="FAN_AVG"
+        ),
         kernel_regularizer=regularizer
     )
     return layer(prev)
