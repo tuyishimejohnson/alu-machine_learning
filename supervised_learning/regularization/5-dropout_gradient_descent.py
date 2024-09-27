3219f89a835d2f61969b171d0c65f218683647a3
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-27 11:58:55 +0200

    Gradient Descent with Dropout

---
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+""" updates the weights and biases of
+a neural network using gradient descent with L2 regularization
+"""
+
+
+import numpy as np
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """
+    Updates the weights of a neural network with Dropout regularization using gradient descent.
+
+    Parameters:
+    Y (np.ndarray): One-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data.
+    weights (dict): Dictionary of the weights and biases of the neural network.
+    cache (dict): Dictionary of the outputs and dropout masks of each layer of the neural network.
+    alpha (float): Learning rate.
+    keep_prob (float): Probability that a node will be kept.
+    L (int): Number of layers of the network.
+
+    Returns:
+    None
+    """
+    m = Y.shape[1]
+    dZ = cache['A' + str(L)] - Y
+
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+
+        dW = (1 / m) * np.dot(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        if i > 1:
+            dA_prev = np.dot(W.T, dZ)
+            dA_prev = dA_prev * cache['D' + str(i - 1)]
+            dA_prev /= keep_prob
+            dZ = dA_prev * (1 - A_prev ** 2)
+
+        weights['W' + str(i)] = W - alpha * dW
+        weights['b' + str(i)] = b - alpha * db
