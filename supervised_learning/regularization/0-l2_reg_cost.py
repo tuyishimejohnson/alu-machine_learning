81a4d0f9cad7a52d2a44036d854a7dd15a760c30
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-27 10:56:36 +0200

    Regularization cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -22,6 +22,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     l2_term = 0
     for i in range(1, L + 1):
         l2_term += np.sum(np.square(weights['W' + str(i)]))
-    
+
     l2_cost = cost + (lambtha / (2 * m)) * l2_term
     return l2_cost
