a093ba934e12978f157eec6dbf16a499a41882e4
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-27 11:41:56 +0200

    Create a Layer with L2 Regularization

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" a function that creates a tensorflow
+layer that includes L2 regularization
+"""
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    args:
+    prev:tensor containing the output of the previous layer
+    n:number of nodes the new layer should contain
+    activation:activation function that should be used on the layer
+    lambtha:L2 regularization parameter
+
+    returns:
+     the output of the new layer
+    """
+    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=
+        tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"),
+        kernel_regularizer=regularizer
+    )
+    return layer(prev)
