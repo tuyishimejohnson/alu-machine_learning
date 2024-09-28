4f470640121dab53061eb6895ff9b9648ed0a4ac
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-28 18:33:29 +0200

    Regularization cost

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -17,9 +17,4 @@ def l2_reg_cost(cost):
     tensor containing the cost of the network
     accounting for L2 regularization
     """
-    l2_term = tf.add_n([
-        tf.nn.l2_loss(var) for var in tf.trainable_variables()
-        if 'kernel' in var.name
-    ])
-    l2_cost = cost + l2_term
-    return l2_cost
+    return cost + tf.losses.get_regularization_losses()
