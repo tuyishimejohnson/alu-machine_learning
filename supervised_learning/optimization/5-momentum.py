2cf789c22ccd4507f3f7dad8a676aa755f7104ee
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-28 10:09:20 +0200

    Momentum

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" that updates a variable using
+the gradient descent with momentum optimization algorithm
+"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    args:
+    alpha:learning rate
+    beta1:momentum weight
+    var:numpy.ndarray containing the variable to be updated
+    grad:numpy.ndarray containing the gradient of var
+    v:previous first moment of var
+    returns:
+     the updated variable and the new moment, respectively
+
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
