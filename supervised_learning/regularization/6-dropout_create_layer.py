17e299598f1789d4d1684358306ba9d4c282b37b
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-28 18:25:00 +0200

    Create a layer of NN

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -17,14 +17,8 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     the output of the new layer
     """
 
-    initializer = tf.contrib.layers.variance_scaling_initializer(
-        mode="FAN_AVG"
-    )
-    layer = tf.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=initializer
-    )
-    output = layer(prev)
-    dropout = tf.nn.dropout(output, keep_prob)
-    return dropout
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init)
+    dropout = tf.layers.Dropout(rate=keep_prob)
+    return dropout(layer(prev))
