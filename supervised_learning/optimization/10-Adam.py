fbfec5ed1ea73f631ddc0b18bff0fcf6a73b8cec
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-28 10:57:49 +0200

    Adam Upgraded

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+""" A function that updates
+a variable using the RMSProp optimization algorithm
+"""
+
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    args:
+    loss is the loss of the network
+    alpha:learning rate
+    beta1:weight used for the first moment
+    beta2:weight used for the second moment
+    epsilon:small number to avoid division by zero
+    returns:
+      the updated variable, the new first moment,
+      and the new second moment, respectively
+
+    """
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
