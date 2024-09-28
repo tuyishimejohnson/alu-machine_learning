a5dc82bc12b5aca9fdc1f919edc79cbd0f35324f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-28 10:59:14 +0200

    Adam Upgraded

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -20,6 +20,8 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
       and the new second moment, respectively
 
     """
-    optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon)
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon
+    )
     train_op = optimizer.minimize(loss)
     return train_op
