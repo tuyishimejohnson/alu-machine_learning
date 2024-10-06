4730d2c05f886c28dad9499126fbecbb92e543ef
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-06 15:25:22 +0200

    Calculate F1 score of each class

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+A function that that calculates the F1
+score of a confusion matrix
+"""
+
+import numpy as np
+sensitivity = __import__("1-sensivity").sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """
+    confusion: confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
+    classes: number of classes
+
+    Returns:
+    a numpy.ndarray of shape (classes,) containing
+    the F1 score of each class
+    Use: You must use sensitivity = __import__('1-sensitivity').sensitivity
+    and precision = __import__('2-precision').precision
+    """
+    recall = sensitivity(confusion)
+
+    prec = precision(confusion)
+
+    f1 = 2 * (prec * recall) / (prec + recall)
+    
+    return f1
