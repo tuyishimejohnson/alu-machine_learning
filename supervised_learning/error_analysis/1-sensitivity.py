97f582c8b10b99261273789aad6d9335ed1c2b02
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-06 14:58:31 +0200

    Sensitivity matrix

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -9,9 +9,7 @@ import numpy as np
 
 def sensitivity(confusion):
     """
-    Calculates the sensitivity for each class in a confusion matrix.
-
-    Parameters:
+    params:
     confusion (np.ndarray): Confusion matrix of shape
     (classes, classes) where row indices represent the
     correct labels and column indices represent the predicted labels.
