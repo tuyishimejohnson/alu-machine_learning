6adc01dac21d4637fcaf6825e3ee94609354f994
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-06 15:18:36 +0200

    Calculate the specificity of each class

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+A function that that calculates the specificity for each class in a confusion matrix:
+"""
+
+import numpy as np
+
+
+def precision(confusion):
+    """
+    confusion: confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
+    classes: number of classes
+
+    Returns:
+    a numpy.ndarray of shape (classes,) containing the specificity of each class
+    """
+    true_pos = np.diag(confusion)
+
+    false_neg = np.sum(confusion, axis=1) - true_pos
+
+    false_pos = np.sum(confusion, axis=0) - true_pos
+
+    true_neg = np.sum(confusion) - (true_pos + false_neg + false_pos)
+    
+    return true_neg / (true_neg + false_pos)
