fdfd8cc033f87905f3e054f7d346db9730ba24de
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-06 15:08:23 +0200

    Calculate precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+A function that calculates the 
+precision for each class in a confusion matrix  
+"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    confusion: confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
+    classes: number of classes
+
+    Returns: 
+    a numpy.ndarray of shape (classes,) containing the precision of each class
+    """
+    true_positives = np.diag(confusion)
+    
+    false_positives = np.sum(confusion, axis=0) - true_positives
+    
+    precision = true_positives / (true_positives + false_positives)
+    
+    return precision
