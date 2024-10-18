0e6382983d66428cf080deae8a639df6f2c5b916
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-18 10:52:53 +0200

    A function that creates a pd dataframe from numpy array

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,28 @@
+""" #!/usr/bin/env python3 """
+"""
+A function def from_numpy(array): that creates a pd.DataFrame
+from a np.ndarray
+"""
+
+import pandas as pd
+import numpy as np
+
+def from_numpy(array):
+    """
+    array: np.ndarray from which you should create the pd.DataFrame
+    The columns of the pd.DataFrame should be labeled in alphabetical order and
+    capitalized. There will not be more than 26 columns.
+
+    Returns:
+    the newly created pd.DataFrame
+    """
+
+    columns = []
+    for i in range(array.shape[1]):
+        columns.append(chr(65 + i))
+
+    return pd.DataFrame(array, columns=columns)
+    
+
+A = np.random.randn(5, 8)
+print(from_numpy(A))
