34e1f000f49c405c27992dcfea67874e9d50e103
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-19 14:22:41 +0200

    Sort dataframe by 'High' column in descending order

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,11 +1,10 @@
-""" #!/usr/bin/env python3 """
+#!/usr/bin/env python3
 
 """  Sort dataframe by 'High' column in descending order """
 
 from_file = __import__('2-from_file').from_file
 
 df = from_file('pipeline/pandas/datasets/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
 df = df.sort_index(by='High', ascending=False)
 
 print(df.head())
