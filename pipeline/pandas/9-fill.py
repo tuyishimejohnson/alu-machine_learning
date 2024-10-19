87f6c9939b03618f489ee71e587fbc5c681dd0ab
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-19 15:02:04 +0200

    script to fill in the missing data points in the pd.DataFrame

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+""" script to fill in the missing data points in the pd.DataFrame """
+
+from_file = __import__('2-from_file').from_file
+
+df = from_file('pipeline/pandas/datasets/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.drop(columns=['Weighted_Price'])
+
+df['Close'] = df['Close'].fillna(method='ffill')
+
+
+df['High'] = df['High'].fillna(df['Close'])
+df['Low'] = df['Low'].fillna(df['Close'])
+df['Open'] = df['Open'].fillna(df['Close'])
+
+df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+
+print(df.head())
+print(df.tail())
