a12a35d2f42519ebfbf41e78c03ffd1a88ff4e44
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-24 22:19:30 +0200

    Add function to retrieve available ships based on passenger count

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+from io import BytesIO
+
+def availableShips(passengerCount):
+    """
+    Method that returns the list of ships
+    that can hold a given number of passengers.
+
+    Args:
+        passengerCount (int): number of passengers
+        to be transported
+
+    Returns:
+        list: List of ship names that can hold the given number of passengers
+    """
+    url_link = 'https://swapi-api.alx-tools.com/api/starships/'
+    list_ships = []
+
+    while url_link:
+        response = requests.get(url_link)
+
+        # Handle error incase request fails
+        if response.status_code != 200:
+            print(f"Failed to retrieve data: {response.status_code}")
+            break
+
+        data = response.json()
+
+        for ship in data['results']:
+            # Skip ships with invalid passenger values
+            if ship["passengers"] not in ["n/a", "unknown", "0", "none"]:
+                try:
+                    ship["passengers"] = ship["passengers"].replace(",", "")
+                    if int(ship['passengers']) >= passengerCount:
+                        list_ships.append(ship['name'])
+                except ValueError:
+                    # Handle unexpected non-numeric passenger values
+                    pass
+
+        # Move to the next page if available
+        url_link = data['next']
+
+    return list_ships
