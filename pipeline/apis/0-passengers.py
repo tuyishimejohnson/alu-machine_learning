30fdd0dfd52a803303a220b6401ef63aa8c0d12f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-25 07:59:06 +0200

    Document the file

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
+
+"""
+A method that returns the list of ships that can
+hold a given number of passengers
+"""
 import requests
 
+
 def availableShips(passengerCount):
+
+    """
+    Arguments:
+    passengerCount: number of passengers
+
+    Returns:
+    If no ship available, return an empty list.
+    """
+
     url = 'https://swapi.dev/api/starships/'
     ships = []
     
@@ -9,7 +24,8 @@ def availableShips(passengerCount):
         response = requests.get(url)
         data = response.json()
         for ship in data['results']:
-            # Some ships may have unknown or non-numeric values in the passengers field, so we need to handle that
+            # Some ships may have unknown or non-numeric values
+            # in the passengers field, so we need to handle that
             try:
                 passengers = ship['passengers'].replace(',', '')  # Remove any commas from the string
                 if int(passengers) >= passengerCount:
