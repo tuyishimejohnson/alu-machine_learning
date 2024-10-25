26e450b23e85cfdd06c886deb66d037aa663acf7
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-25 08:02:03 +0200

    Remove unneccessary comments

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -24,8 +24,6 @@ def availableShips(passengerCount):
         response = requests.get(url)
         data = response.json()
         for ship in data['results']:
-            # Some ships may have unknown or non-numeric values
-            # in the passengers field, so we need to handle that
             try:
                 passengers = ship['passengers'].replace(',', '')    
                 if int(passengers) >= passengerCount:
