607d23a81003c31a8089632e171923ef4b33cca5
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-25 17:34:30 +0200

    changes made

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
-Prints the location of a user
-'''
+"""
+    script that prints the location of a specific user:
+"""
 
 
 import sys
@@ -15,34 +15,32 @@ def get_user_location(api_url):
 
     :param api_url: The API URL for the user
     """
-    try:
-        response = requests.get(api_url)
+    response = requests.get(url)
 
-        if response.status_code == 200:
-            user_data = response.json()
-            location = user_data.get('location')
-            if location:
-                print(location)
-            else:
-                print('Location not available')
-        elif response.status_code == 404:
-            print('Not found')
-        elif response.status_code == 403:
-            reset_time = int(
-                response.headers.get('X-RateLimit-Reset', time.time()))
-            current_time = int(time.time())
-            wait_time = (reset_time - current_time) // 60
-            print('Reset in {} min'.format(wait_time))
-        else:
-            print('Error: {}'.format(response.status_code))
-    except requests.RequestException as e:
-        print('An error occurred: {}'.format(e))
+    if response.status_code == 200:
+        # Successfully retrieved data; return location if available
+        data = response.json()
+        return data.get('location', "Not found")
+    
+    elif response.status_code == 404:
+        # User not found
+        return "Not found"
+    
+    elif response.status_code == 403:
+        # Rate limit exceeded
+        reset_timestamp = int(response.headers.get('X-Ratelimit-Reset', 0))
+        reset_in_minutes = (datetime.fromtimestamp(reset_timestamp) - datetime.now()).total_seconds() // 60
+        return f"Reset in {int(reset_in_minutes)} min"
+
+    # For other errors, return a generic message
+    return "Error retrieving data"
 
 
 if __name__ == '__main__':
+    # Ensure that the URL argument is provided
     if len(sys.argv) != 2:
-        print('Usage: ./2-user_location.py <api_url>')
+        print("Usage: ./2-user_location.py <GitHub API user URL>")
         sys.exit(1)
-
-    api_url = sys.argv[1]
-    get_user_location(api_url)
+    
+    url = sys.argv[1]
+    print(get_user_location(url))
