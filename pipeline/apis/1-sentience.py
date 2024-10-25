a40d6931a2447b75cba4c93ab6ee2b2e1176631e
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-25 16:47:47 +0200

    Refactor the codes

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,48 +1,29 @@
 #!/usr/bin/env python3
-
 """
-A method that returns the list of names
-of the home planets of all sentient species.
+    Return the list of names of the home
+    planets of all sentient species.
 """
-
 
 import requests
 
+
 def sentientPlanets():
-    """
-    Arguments:
-    passengerCount: number of passengers
-
-    Returns:
-    If no ship available, return an empty list.
-    """
-    url_link = 'https://swapi.dev/api/species/'
-    planets = set()
-
-    while url_link:
-        response = requests.get(url_link)
-
-        if response.status_code != 200:
-            print(f"Failed to retrieve data: {response.status_code}")
-            break
-
-        data = response.json()
-
-        for species in data['results']:
-
-            if species['classification'] == 'sentient':
-                homeworld_url = species['homeworld']
-
-                if homeworld_url:
-                    homeworld_response = requests.get(homeworld_url)
-                    
-                    if homeworld_response.status_code == 200:
-                        homeworld_data = homeworld_response.json()
-                        planets.add(homeworld_data['name'])
-                    else:
-                        print(f"Failed to retrieve homeworld data for {species['name']}")
-                else:
-                    planets.add("unknown")
-        url_link = data['next']
-
-    return list(planets)
+    '''
+    Return the list of names of the home
+    planets of all sentient species.
+    '''
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
