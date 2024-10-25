700a209e8e8becc9bee1927dff08f301326ca88f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-25 16:37:10 +0200

    A method that returns the list of names of the home planets of all sentient species

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+A method that returns the list of names
+of the home planets of all sentient species.
+"""
+
+import requests
+
+def sentientPlanets():
+    """
+    Method to retrieve the list of names of home planets for all sentient species
+    from the SWAPI (Star Wars API).
+    
+    Returns:
+        list: List of names of home planets of all sentient species. 
+              If no planet is found for a species, "unknown" is used instead.
+    """
+    url_link = 'https://swapi.dev/api/species/'
+    planets = set()
+
+    while url_link:
+        response = requests.get(url_link)
+
+        if response.status_code != 200:
+            print(f"Failed to retrieve data: {response.status_code}")
+            break
+
+        data = response.json()
+
+        for species in data['results']:
+
+            if species['classification'] == 'sentient':
+                homeworld_url = species['homeworld']
+
+                if homeworld_url:
+                    homeworld_response = requests.get(homeworld_url)
+                    
+                    if homeworld_response.status_code == 200:
+                        homeworld_data = homeworld_response.json()
+                        planets.add(homeworld_data['name'])
+                    else:
+                        print(f"Failed to retrieve homeworld data for {species['name']}")
+                else:
+                    planets.add("unknown")
+
+        # Move to the next page if available
+        url_link = data['next']
+
+    return list(planets)
