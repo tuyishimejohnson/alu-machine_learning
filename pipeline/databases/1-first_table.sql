0312f217cd7fd20be31afd9d17aaab107e8653ef
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-11-01 08:39:03 +0200

    Create a table called first_table

---
+++ pipeline/databases/1-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table called first_table in the current database in your MySQL server
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256),
+    age INT NOT NULL
+);
