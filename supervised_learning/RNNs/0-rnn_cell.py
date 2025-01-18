16337c44afb5b535248b21d21b425b7668cb8f77
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-01-18 11:58:59 +0200

    Remove unneccessary comments

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 class RNNCell:
     def __init__(self, i, h, o):
         """
@@ -12,10 +13,10 @@ class RNNCell:
         - h (int): Dimensionality of the hidden state.
         - o (int): Dimensionality of the outputs.
         """
-        self.Wh = np.random.normal(size=(i + h, h))  # Weights for hidden state and input concatenation
-        self.Wy = np.random.normal(size=(h, o))      # Weights for the output
-        self.bh = np.zeros((1, h))                   # Bias for the hidden state
-        self.by = np.zeros((1, o))                   # Bias for the output
+        self.Wh = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """
@@ -29,13 +30,8 @@ class RNNCell:
         - h_next (numpy.ndarray): Next hidden state, shape (m, h).
         - y (numpy.ndarray): Output of the cell, shape (m, o).
         """
-        # Concatenate h_prev and x_t along the last axis
         concat = np.concatenate((h_prev, x_t), axis=1)
-
-        # Compute next hidden state using tanh activation
         h_next = np.tanh(np.matmul(concat, self.Wh) + self.bh)
-
-        # Compute output using softmax activation
         y_linear = np.matmul(h_next, self.Wy) + self.by
         y = np.exp(y_linear) / np.sum(np.exp(y_linear), axis=1, keepdims=True)
 
