a52f567fdbf4747ed7bdfd13092fbb5817e3ac87
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-01-18 13:43:58 +0200

    Class BidirectionalCell

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+BidirectionalCell that represents
+a bidirectional cell of an RNN
+"""
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    Represents a bidirectional cell of an RNN.
+
+    Attributes:
+    - Whf: Weight matrix for the forward hidden states.
+    - bhf: Bias vector for the forward hidden states.
+    - Whb: Weight matrix for the backward hidden states.
+    - bhb: Bias vector for the backward hidden states.
+    - Wy: Weight matrix for the outputs.
+    - by: Bias vector for the outputs.
+    """
+
+    def __init__(self, i, h, o):
+        """
+        Class constructor.
+
+        Parameters:
+        - i: Dimensionality of the data.
+        - h: Dimensionality of the hidden states.
+        - o: Dimensionality of the outputs.
+        """
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.bhf = np.zeros((1, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.bhb = np.zeros((1, h))
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Calculates the hidden state in the forward direction for one time step.
+
+        Parameters:
+        - h_prev: numpy.ndarray of shape (m, h), previous hidden state.
+        - x_t: numpy.ndarray of shape (m, i), data input for the cell.
+
+        Returns:
+        - h_next: numpy.ndarray of shape (m, h), the next hidden state.
+        """
+        # Concatenate h_prev and x_t
+        concatenated = np.concatenate((h_prev, x_t), axis=1)
+        # Compute the next hidden state
+        h_next = np.tanh(np.matmul(concatenated, self.Whf) + self.bhf)
+        return h_next
