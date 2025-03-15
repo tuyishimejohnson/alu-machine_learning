35c5fc0b8d50c8105bf85496d50f507261d433ca
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-03-15 19:30:27 +0200

    q table

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Initialize the q-table.
+"""
+
+def q_init(env):
+    """Initializes the Q-table.
+
+    Args:
+        env: The FrozenLakeEnv instance.
+
+    Returns:
+        The Q-table as a numpy.ndarray of zeros.
+    """
+    return np.zeros([env.observation_space.n, env.action_space.n])
